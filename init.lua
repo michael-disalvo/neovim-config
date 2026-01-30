008ae90f846e3c8f61e3a8c173db4b7195c363b2
Michael DiSalvo <mtdisalvo3@gmail.com>
2026-01-29 19:46:18 -0500

    link wsl clipboard and create keymap

--- init.lua
+++ init.lua
@@ -24,6 +24,32 @@ vim.keymap.set("i", "jj", "<ESC>")
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
+vim.keymap.set("v", "Y", '"+y')
+
+vim.g.clipboard = {
+  name = "WslClipboard",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = {
+      "powershell.exe",
+      "-NoLogo",
+      "-NoProfile",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    ["*"] = {
+      "powershell.exe",
+      "-NoLogo",
+      "-NoProfile",
+      "-c",
+      '[Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  },
+  cache_enabled = 0,
+}
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking text",
