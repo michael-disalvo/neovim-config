01b003b84f43308a2c90a3c8e0811e2745dc9d68
michael-disalvo <mtdisalvo3@gmail.com>
2026-02-20 11:26:57 -0500

    handle VimResized event

--- plugins/present.nvim/lua/present.lua
+++ plugins/present.nvim/lua/present.lua
@@ -61,28 +61,11 @@ local parse_slides = function(lines)
   -- nothing
 end
 
-M.start_presentation = function(opts)
-  opts = opts or {}
-  opts.bufnr = opts.bufnr or 0
-
-  local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
-  local parsed = parse_slides(lines)
-
-  -- local win_config = {
-  --   relative = "editor",
-  --   width = width,
-  --   height = height,
-  --   row = row,
-  --   col = col,
-  --   style = "minimal",
-  --   border = { " ", " ", " ", " ", " ", " ", " ", " ", }
-  -- }
-
+local create_window_configurations = function() 
   local width = vim.o.columns
   local height = vim.o.lines
 
-  ---@type keyset.win_config
-  local windows = {
+  return {
     background = {
       relative = "editor",
       width = width,
@@ -105,7 +88,7 @@ M.start_presentation = function(opts)
     body = {
       relative = "editor",
       width = width - 8,
-      height = height - 5,
+      height = height - 6,
       style = "minimal",
       border = { " ", " ", " ", " ", " ", " ", " ", " " },
       col = 8,
@@ -113,6 +96,28 @@ M.start_presentation = function(opts)
     }
     -- footer = {}
   }
+end
+
+
+M.start_presentation = function(opts)
+  opts = opts or {}
+  opts.bufnr = opts.bufnr or 0
+
+  local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
+  local parsed = parse_slides(lines)
+  local current_slide = 1
+
+  -- local win_config = {
+  --   relative = "editor",
+  --   width = width,
+  --   height = height,
+  --   row = row,
+  --   col = col,
+  --   style = "minimal",
+  --   border = { " ", " ", " ", " ", " ", " ", " ", " ", }
+  -- }
+
+  local windows = create_window_configurations()
 
   local background = create_floating_window(windows.background)
   local header = create_floating_window(windows.header)
@@ -122,6 +127,7 @@ M.start_presentation = function(opts)
   vim.bo[body.buf].filetype = "markdown"
 
   local set_slide_content = function(idx) 
+    local width = vim.o.columns
     local slide = parsed.slides[idx]
     local padding = string.rep(" ", (width - #slide.title) / 2)
     local title = padding .. slide.title
@@ -129,7 +135,6 @@ M.start_presentation = function(opts)
     vim.api.nvim_buf_set_lines(body.buf, 0, -1, false, slide.body)
   end
 
-  local current_slide = 1
   vim.keymap.set("n", "n", function() 
     current_slide = math.min(current_slide + 1, #parsed.slides)
     set_slide_content(current_slide)
@@ -170,6 +175,23 @@ M.start_presentation = function(opts)
         vim.opt[option] = config.original
       end
     end
+  })
+
+  vim.api.nvim_create_autocmd("VimResized", {
+    group = vim.api.nvim_create_augroup("present-resized", {}),
+    callback = function() 
+      if not vim.api.nvim_win_is_valid(body.win) or body.win == nil then
+        return
+      end
+
+      local updated = create_window_configurations()
+      vim.api.nvim_win_set_config(header.win, updated.header)
+      vim.api.nvim_win_set_config(background.win, updated.background)
+      vim.api.nvim_win_set_config(body.win, updated.body)
+
+      set_slide_content(current_slide)
+      
+    end,
   })
 
   set_slide_content(current_slide)
