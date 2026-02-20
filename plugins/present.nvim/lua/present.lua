a786adae0565f468d044d0d0d0d959f162025f6d
michael-disalvo <mtdisalvo3@gmail.com>
2026-02-20 14:41:39 -0500

    add footer to presentation

--- plugins/present.nvim/lua/present.lua
+++ plugins/present.nvim/lua/present.lua
@@ -5,12 +5,12 @@ M.setup = function()
 end
 
 
-local function create_floating_window(config)
+local function create_floating_window(config, enter)
   opts = opts or {}
 
   local buf = vim.api.nvim_create_buf(false, true)
 
-  local win = vim.api.nvim_open_win(buf, true, config)
+  local win = vim.api.nvim_open_win(buf, enter or false, config)
 
   return {
     buf = buf,
@@ -65,6 +65,10 @@ local create_window_configurations = function()
   local width = vim.o.columns
   local height = vim.o.lines
 
+  local header_height = 1 + 2 -- 1 + border
+  local footer_height = 1 -- 1, no border
+  local body_height = height - header_height - footer_height - 2 - 1-- sub 2 for border
+
   return {
     background = {
       relative = "editor",
@@ -88,14 +92,38 @@ local create_window_configurations = function()
     body = {
       relative = "editor",
       width = width - 8,
-      height = height - 6,
+      height = body_height,
       style = "minimal",
       border = { " ", " ", " ", " ", " ", " ", " ", " " },
       col = 8,
       row = 4,
-    }
-    -- footer = {}
+    },
+    footer = {
+      relative = "editor",
+      width = width,
+      height = 1,
+      style = "minimal",
+      col = 0,
+      row = height - 1,
+      zindex = 3,
+    },
   }
+end
+
+local state = {
+  current_slide = 1,
+  parsed = {},
+  floats = {},
+}
+
+local foreach_float = function(cb) 
+  for name, float in pairs(state.floats) do
+    cb(name, float)
+  end
+end
+
+local present_keymap = function(mode, key, callback)
+  vim.keymap.set(mode, key, callback, { buffer = state.floats.body.buf })
 end
 
 
@@ -104,8 +132,8 @@ M.start_presentation = function(opts)
   opts.bufnr = opts.bufnr or 0
 
   local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
-  local parsed = parse_slides(lines)
-  local current_slide = 1
+  state.parsed = parse_slides(lines)
+  state.title = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(opts.bufnr), ":t")
 
   -- local win_config = {
   --   relative = "editor",
@@ -119,41 +147,38 @@ M.start_presentation = function(opts)
 
   local windows = create_window_configurations()
 
-  local background = create_floating_window(windows.background)
-  local header = create_floating_window(windows.header)
-  local body = create_floating_window(windows.body)
+  state.floats.background = create_floating_window(windows.background)
+  state.floats.header = create_floating_window(windows.header)
+  state.floats.footer = create_floating_window(windows.footer)
+  state.floats.body = create_floating_window(windows.body, true)
 
-  vim.bo[header.buf].filetype = "markdown"
-  vim.bo[body.buf].filetype = "markdown"
+  foreach_float(function(_, float) vim.bo[float.buf].filetype = "markdown" end)
 
   local set_slide_content = function(idx) 
     local width = vim.o.columns
-    local slide = parsed.slides[idx]
+    local slide = state.parsed.slides[idx]
     local padding = string.rep(" ", (width - #slide.title) / 2)
     local title = padding .. slide.title
-    vim.api.nvim_buf_set_lines(header.buf, 0, -1, false, { title })
-    vim.api.nvim_buf_set_lines(body.buf, 0, -1, false, slide.body)
+
+    local footer = string.format(" %d / %d | %s", state.current_slide, #state.parsed.slides, state.title)
+    vim.api.nvim_buf_set_lines(state.floats.header.buf, 0, -1, false, { title })
+    vim.api.nvim_buf_set_lines(state.floats.body.buf, 0, -1, false, slide.body)
+    vim.api.nvim_buf_set_lines(state.floats.footer.buf, 0, -1, false, { footer })
   end
 
-  vim.keymap.set("n", "n", function() 
-    current_slide = math.min(current_slide + 1, #parsed.slides)
-    set_slide_content(current_slide)
-  end, {
-    buffer = body.buf -- sets these keymaps to only work inside the buffer attached to the floating window
-  })
+  present_keymap("n", "n", function()
+    state.current_slide = math.min(state.current_slide + 1, #state.parsed.slides)
+    set_slide_content(state.current_slide)
+  end)
 
-  vim.keymap.set("n", "p", function() 
-    current_slide = math.max(current_slide - 1 , 1)
-    set_slide_content(current_slide)
-  end, {
-    buffer = body.buf
-  })
+  present_keymap("n", "p", function() 
+    state.current_slide = math.max(state.current_slide - 1 , 1)
+    set_slide_content(state.current_slide)
+  end)
 
-  vim.keymap.set("n", "q", function() 
-    vim.api.nvim_win_close(header.win, true)
-    vim.api.nvim_win_close(body.win, true)
-    vim.api.nvim_win_close(background.win, true)
-  end, { buffer = body.buf })
+  present_keymap("n", "q", function() 
+    foreach_float(function(_, float) vim.api.nvim_win_close(float.win, true) end)
+  end)
 
   local restore = {
     cmdheight = {
@@ -168,7 +193,7 @@ M.start_presentation = function(opts)
   end
 
   vim.api.nvim_create_autocmd("BufLeave", {
-    buffer = body.buf,
+    buffer = state.floats.body.buf,
     callback = function() 
       -- reset the values when we are done with the presentation
       for option, config in pairs(restore) do
@@ -180,21 +205,19 @@ M.start_presentation = function(opts)
   vim.api.nvim_create_autocmd("VimResized", {
     group = vim.api.nvim_create_augroup("present-resized", {}),
     callback = function() 
-      if not vim.api.nvim_win_is_valid(body.win) or body.win == nil then
+      if not vim.api.nvim_win_is_valid(state.floats.body.win) or state.floats.body.win == nil then
         return
       end
 
       local updated = create_window_configurations()
-      vim.api.nvim_win_set_config(header.win, updated.header)
-      vim.api.nvim_win_set_config(background.win, updated.background)
-      vim.api.nvim_win_set_config(body.win, updated.body)
+      foreach_float(function(name, _) vim.api.nvim_win_set_config(state.floats[name].win, updated[name]) end)
 
-      set_slide_content(current_slide)
+      set_slide_content(state.current_slide)
       
     end,
   })
 
-  set_slide_content(current_slide)
+  set_slide_content(state.current_slide)
 end
 
 -- parse_slides( {
@@ -204,6 +227,6 @@ end
 --   "this is another",
 -- })
 
-M.start_presentation( { bufnr = 5 } )
+M.start_presentation( { bufnr = 6 } )
 
 return M
