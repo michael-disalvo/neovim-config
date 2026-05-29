a8607bc41f5609313f5adae66cea1313c11cc40c
michael-disalvo <mtdisalvo3@gmail.com>
2026-05-29 09:21:16 -0400

    treesitter: change name for upgrade

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,17 +1,17 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = master, 
-    lazy = false, 
+    "nvim-treesitter/nvim-treesitter",
+    branch = master,
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      require('nvim-treesitter.configs').setup {
-	    ensure_installed = { "c", "lua", "vim", "rust", "c", "go", "python"},
+      require('nvim-treesitter.config').setup {
+        ensure_installed = { "c", "lua", "vim", "rust", "c", "go", "python" },
         highlight = {
           enable = true,
           disable = { "rust", "go" }
         },
-     }
+      }
     end,
   }
 }
